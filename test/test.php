699dd299583ceb865e631050dc5eb085b5310002
高子翔 <kao921019@gmail.com>
2023-09-27 02:10:32 +0800

    no message

--- test/test.php
+++ test/test.php
@@ -10,42 +10,69 @@
         </body>
         <?php
         session_start();
-        
-        $times=0;
-        $num;
-        $str="";
-        if(isset($_POST['submit']))
+        $link=@mysqli_connect('localhost','root','','107php');
+        /*if(isset($link))
         {
-        for($i=0;$i<10;$i++)
-        {
-            $array=array();
-            while(count($array)<3)
-            {
-                $num=rand(1,9);
-                if(!in_array($num,$array))
-                {
-                    $array[]=$num;
-                }
-            }
-            $_SESSION['array']=$array;
-        if(abs($_SESSION['array'][1]-$_SESSION['array'][0]==abs($_SESSION['array'][2]-$_SESSION['array'][1])))
-        {
-            $times++;
-            $str=$times." = ".$_SESSION['array'][0].",".$_SESSION['array'][1].",".$_SESSION['array'][2];
-            echo $str;
-            echo "</br>";
-            break;
+            echo "成功";
         }
         else
         {
-            $times++;
-            $str=$times." = ".$_SESSION['array'][0].",".$_SESSION['array'][1].",".$_SESSION['array'][2];
-            echo $str;
-            echo "</br>";
+            echo "失敗";
+        }*/
+        $times=0;
+        $num;
+        $str="";
+        
+        if(isset($_POST['submit']))
+        {
+            for($i=0;$i<10;$i++)
+            {
+                $array=array();
+                $numstr="";
+                while(count($array)<3)
+                {
+                    $num=rand(1,9);
+                    if(!in_array($num,$array))
+                    {
+                        $array[]=$num;
+                    }
+                }
+                foreach($array as $a)
+                {
+                    $numstr.=$a;
+                }
+                if(abs($array[1]-$array[0])==abs($array[2]-$array[1]))
+                {
+                    $times++;
+                    $str=$times." = ".$array[0].",".$array[1].",".$array[2];
+                    echo $str;
+                    echo "</br>";
+                    $sqlturn="INSERT INTO `mydetail`(`id`, `turn`, `rec`)";
+                    $sqlturn.="VALUES (NOW(),'$times','$numstr')";
+                    if(mysqli_query($link,$sqlturn)){};
+                    break;
+                }
+                else
+                {
+                    $times++;
+                    $str=$times." = ".$array[0].",".$array[1].",".$array[2];
+                    echo $str;
+                    echo "</br>";
+                    $sqlturn="INSERT INTO `mydetail`(`id`, `turn`, `rec`)";
+                    $sqlturn.="VALUES (NOW(),'$times','$numstr')";
+                    if(mysqli_query($link,$sqlturn)){};
+                }
+                sleep(0.1);
+                
+                continue;
+            }
+            echo "總共測試".$times."次或是找到數字";
+            $sql="INSERT INTO `mymaster`(`id`, `freq`)"; 
+            $sql.="VALUES (NOW(),'$times')";
+            if(mysqli_query($link,$sql)){};
         }
-        }
-        echo "總共測試".$times."次或是找到數字";
-        }
+        session_destroy();
+        mysqli_close($link);
         ?>
     </haed>
 </html>
